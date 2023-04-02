6d5463f3cc0833a91cf6519a9fbee05e17dbbf98
Frosenow <filip.potepa@gmail.com>
2023-04-02 14:55:22 +0200

    changed folders hierarchy

--- app/src/server.js
+++ app/src/server.js
@@ -5,7 +5,7 @@ const jimp = require('jimp')
 
 // Default setup 
 const app = express()
-app.use(express.static("public"))
+app.use(express.static("/public"))
 app.use(fileUpload())
 // app.use(express.urlencoded({extended: true}))
 
